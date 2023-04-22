404fd2ba7db4c94b6cf659951d0169d59519c068
shomrkm <shochan1985@gmail.com>
2023-04-22 14:30:14 +0900

    feat: Add csp sample codes.

--- server.js
+++ server.js
@@ -4,9 +4,19 @@ const api = require('./routes/api');
 const app = express();
 const PORT = 3000;
 
+app.set("view engine", "ejs");
+
 app.use(express.static("public"));
 
 app.use("/api", api)
+
+app.get("/csp", (req, res) => {
+    // 明示的に unsafe-inline が指定されて居ないページでは、HTML内のインラインスクリプトなどは実行されない
+    res.header("Content-Security-Policy", "script-src 'self'");
+
+    // デフォルトだと views フォルダからの相対パスがレンダリング対象となる
+    res.render("csp");
+});
 
 app.listen(PORT, ()=> {
     console.log(`Server is running on http//localhost:${PORT}`)
