7f4d10ec7262a8ef5991ffc4dcf9604b0c03536b
shomrkm <shochan1985@gmail.com>
2023-05-08 08:09:27 +0900

    feat: Add  "Create Accont" page.

--- server.js
+++ server.js
@@ -38,6 +38,14 @@ app.get("/csp", (req, res) => {
     res.render("csp", { nonce: nonceValue });
 });
 
+// フォームの内容を解析して req.body へ格納する
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/signup", (req, res) => {
+    console.log(req.body);
+    res.send("アカウント登録しました。");
+});
+
 app.listen(PORT, ()=> {
     console.log(`Server is running on http//localhost:${PORT}`)
 });
